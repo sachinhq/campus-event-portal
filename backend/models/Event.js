ad30f2d51e0c74a0213a12a3bb9b80033137ef80
Sachin Kumar <sachinkumar@Sachins-MacBook-Air.local>
2025-12-13 11:58:56 +0530

    backend setup completed

---
+++ backend/models/Event.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: String,
+  date: String,
+  description: String
+});
+
+module.exports = mongoose.model('Event', eventSchema, 'Events');
+
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const PORT = 3001;
+
+console.log("SERVER FILE LOADED");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// TEST ROUTE
+app.get('/test', (req, res) => {
+  res.status(200).json({ ok: true });
+});
+
+// EVENTS ROUTE
+const eventRoutes = require('./routes/events');
+app.use('/api/events', eventRoutes);
+
+// DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
